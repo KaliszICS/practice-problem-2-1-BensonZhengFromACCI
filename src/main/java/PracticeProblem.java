0dce31d26c54a14a17b4ca842217e11ef4f4faa8
BensonZhengFromACCI <343804126@tdsb.ca>
2026-03-10 15:21:46 +0000

    visagaan is bad at coding

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,5 +5,24 @@ public class PracticeProblem {
 	}
 
 	//Create your functions here
+	public static int sum(int num1, int num2) {
+	int result = num1 + num2;
+	return result;
+}
+
+	public static int difference(int num1, int num2) {
+	int result = num1 - num2;
+	return result;
+	}
+
+	public static double product(double num1, double num2) {
+	double result = num1 * num2;
+	return result;
+	}
+
+	public static String removeFirst(String word1) {
+	String result = word1.substring(1);
+	return result;
+}
 
 }
